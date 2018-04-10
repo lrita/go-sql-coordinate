a51294e09ca1b3f4dd94dc17475f4de35579870b
lrita <lrita@163.com>
2018-04-10 22:36:49 +0800

    initial commit

---
+++ keepalived/keepalived.go
@@ -0,0 +1,348 @@
+package keepalived
+
+import (
+	"database/sql"
+	"database/sql/driver"
+	"errors"
+	"sync"
+	"time"
+
+	"github.com/go-sql-driver/mysql"
+)
+
+// Keepalived provides a object which can elect a leader instance by using MySQL.
+type Keepalived struct {
+	mu     sync.Mutex
+	cfg    *Config
+	epoch  chan *Epoch
+	resign chan struct{}
+	dying  chan struct{}
+}
+
+const (
+	defaultTTLPeriod = 3
+	defaultTTL       = 15 * time.Second
+)
+
+// Event represents a event in an epoch.
+type Event int8
+
+const (
+	LeaderChange Event = iota
+	NetworkFault
+	UnknownFault
+)
+
+type Epoch struct {
+	Event   Event
+	Session string
+}
+
+// CreateTable create the MySQL table for election
+func CreateTable(table string, db *sql.DB) (err error) {
+	_, err = db.Exec("CREATE TABLE IF NOT EXISTS `" + table + "` (`election` VARCHAR(255) NOT NULL,`lock_session` VARCHAR(255) NOT NULL,`last_index` BIGINT NOT NULL,`create_at` DATETIME NOT NULL,PRIMARY KEY (`election`))ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;")
+	return
+}
+
+// NewKeepalived create a Keepalived object.
+func NewKeepalived(cfg Config) (*Keepalived, error) {
+	if err := cfg.Validate(); err != nil {
+		return nil, err
+	}
+	return &Keepalived{
+		cfg:    &cfg,
+		resign: make(chan struct{}),
+	}, nil
+}
+
+// Config represents the configuration of the keepalived needed
+type Config struct {
+	Table         string
+	Election      string
+	Session       string
+	EpochChanSize int
+	TTL           time.Duration
+	DB            *sql.DB
+}
+
+// Validate validate each fields of the Config
+func (c *Config) Validate() error {
+	if c.Table == "" {
+		return errors.New("keepalived: empty table")
+	}
+	if c.Election == "" {
+		return errors.New("keepalived: empty election")
+	}
+	if c.Session == "" {
+		return errors.New("keepalived: empty session")
+	}
+	if c.TTL == 0 {
+		c.TTL = defaultTTL
+	}
+	if c.DB == nil {
+		return errors.New("keepalived: empty db")
+	}
+	return nil
+}
+
+// Election launch a election by the given configuration. When you close the
+// cfg.Stop, it will exit the election
+func (k *Keepalived) Election() <-chan *Epoch {
+	k.mu.Lock()
+	defer k.mu.Unlock()
+	if k.dying != nil {
+		return k.epoch
+	}
+
+	k.dying = make(chan struct{})
+	k.epoch = make(chan *Epoch, k.cfg.EpochChanSize)
+
+	go func(dying <-chan struct{}, epoch chan *Epoch) {
+		for {
+			k.campaign(dying, epoch)
+			select {
+			case <-dying:
+				close(epoch)
+				return
+			case <-time.After(k.cfg.TTL/defaultTTLPeriod + time.Second):
+			case <-k.resign:
+			}
+		}
+	}(k.dying, k.epoch)
+
+	return k.epoch
+}
+
+// Exit notifies the candidate to exit the election.
+func (k *Keepalived) Exit() {
+	k.mu.Lock()
+	defer k.mu.Unlock()
+	if k.dying == nil {
+		return
+	}
+	close(k.dying)
+	k.dying = nil
+}
+
+// Resign notifies the candidate to resign the election once.
+func (k *Keepalived) Resign() {
+	k.mu.Lock()
+	defer k.mu.Unlock()
+	if k.dying == nil {
+		return
+	}
+	k.resign <- struct{}{}
+}
+
+func (k *Keepalived) campaign(dying <-chan struct{}, epoch chan<- *Epoch) {
+	var (
+		challenge, netfault bool
+		lastindex, count    int64
+		lastsession         string
+		ttl                 = k.cfg.TTL / defaultTTLPeriod
+		tick                = time.NewTimer(0)
+	)
+
+	defer func() {
+		tick.Stop()
+		if lastsession == k.cfg.Session {
+			epoch <- &Epoch{Event: LeaderChange}
+			k.stepdown()
+		}
+	}()
+
+	for {
+		select {
+		case <-tick.C:
+		case <-dying:
+			return
+		case <-k.resign:
+			return
+		}
+
+		var (
+			session string
+			index   int64
+			err     error
+		)
+
+		if lastsession == k.cfg.Session {
+			index, session, err = k.lease()
+		} else {
+			index, session, err = k.acquire(lastindex, challenge)
+		}
+
+		switch err {
+		case nil:
+			netfault = false
+			switch {
+			case session != lastsession:
+				epoch <- &Epoch{Event: LeaderChange, Session: session}
+				fallthrough
+			case index != lastindex:
+				lastsession, challenge, lastindex, count = session, false, index, 0
+			default:
+				count++
+				if count >= defaultTTLPeriod-1 {
+					challenge = true
+				}
+			}
+		case driver.ErrBadConn, mysql.ErrInvalidConn:
+			if !netfault {
+				epoch <- &Epoch{Event: NetworkFault}
+			}
+			lastsession, challenge, netfault, lastindex, count = "", false, true, 0, 0
+		case sql.ErrNoRows:
+			return
+		default:
+			epoch <- &Epoch{Event: UnknownFault}
+			return
+		}
+		tick.Reset(ttl)
+	}
+}
+
+func (k *Keepalived) acquire(lastindex int64, challenge bool) (index int64, session string, err error) {
+	var (
+		tx       *sql.Tx
+		result   sql.Result
+		affected int64
+	)
+
+	if tx, err = k.cfg.DB.Begin(); err != nil {
+		return
+	}
+
+	defer func() {
+		if err == nil {
+			err = tx.Commit()
+		} else {
+			tx.Rollback()
+		}
+	}()
+
+	// lock the mysql row
+	row := tx.QueryRow(
+		"SELECT `lock_session`, `last_index` FROM `"+
+			k.cfg.Table+
+			"` WHERE `election` = ? LIMIT 1 FOR UPDATE;",
+		k.cfg.Election)
+	if err = row.Scan(&session, &index); err != nil && err != sql.ErrNoRows {
+		return
+	}
+
+	switch session {
+	case "":
+		// no one hold the election
+		n := time.Now()
+		result, err = tx.Exec(
+			"INSERT INTO `"+k.cfg.Table+
+				"` (`election`, `lock_session`, `last_index`, `create_at`) VALUES(?,?,?,?)"+
+				" ON DUPLICATE KEY UPDATE `lock_session` = ?, `last_index`=`last_index`+1, `create_at` = ?;",
+			k.cfg.Election, k.cfg.Session, int64(1), n, k.cfg.Session, n)
+	case k.cfg.Session:
+		// the current session is same with us, we hold this election directly
+		result, err = tx.Exec(
+			"UPDATE `"+
+				k.cfg.Table+
+				"` SET `last_index`=`last_index`+1 WHERE `election` = ?;",
+			k.cfg.Election)
+	default:
+		if !challenge || lastindex != index { // someone is helding the election
+			return
+		}
+		// someone lost this election
+		result, err = tx.Exec(
+			"UPDATE `"+
+				k.cfg.Table+
+				"` SET `lock_session`=?, `last_index`=`last_index`+1, `create_at`=? WHERE `election` = ?;",
+			k.cfg.Session, time.Now(), k.cfg.Election)
+	}
+
+	if err != nil {
+		return
+	}
+
+	if affected, err = result.RowsAffected(); err != nil {
+		return
+	}
+
+	if affected != 0 {
+		index++
+		session = k.cfg.Session
+	}
+	return
+}
+
+func (k *Keepalived) lease() (index int64, session string, err error) {
+	var (
+		tx       *sql.Tx
+		result   sql.Result
+		affected int64
+	)
+
+	if tx, err = k.cfg.DB.Begin(); err != nil {
+		return
+	}
+
+	defer func() {
+		if err == nil {
+			err = tx.Commit()
+		} else {
+			tx.Rollback()
+		}
+	}()
+
+	row := tx.QueryRow(
+		"SELECT `lock_session`, `last_index` FROM `"+k.cfg.Table+
+			"` WHERE `election` = ? LIMIT 1 FOR UPDATE;",
+		k.cfg.Election)
+	if err = row.Scan(&session, &index); err != nil {
+		return
+	}
+
+	if session != k.cfg.Session {
+		err = sql.ErrNoRows
+		return
+	}
+
+	result, err = tx.Exec(
+		"UPDATE `"+k.cfg.Table+
+			"` SET `last_index`=`last_index`+1 WHERE `election` = ?;",
+		k.cfg.Election)
+	if err != nil {
+		return
+	}
+
+	if affected, err = result.RowsAffected(); err != nil {
+		return
+	}
+
+	if affected == 0 {
+		err = errors.New("keepalived: lease missing")
+	}
+	return
+}
+
+func (k *Keepalived) stepdown() {
+	var (
+		tx  *sql.Tx
+		err error
+	)
+
+	if tx, err = k.cfg.DB.Begin(); err != nil {
+		return
+	}
+
+	defer func() {
+		if err == nil {
+			tx.Commit()
+		} else {
+			tx.Rollback()
+		}
+	}()
+
+	_, err = tx.Exec(
+		"UPDATE `"+k.cfg.Table+"` SET `lock_session`='' WHERE `election` = ?;",
+		k.cfg.Election)
+}
---
+++ keepalived/keepalived_test.go
@@ -0,0 +1,196 @@
+package keepalived
+
+import (
+	"database/sql"
+	"os"
+	"sync"
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/suite"
+)
+
+var dsn = "root@tcp(127.0.0.1:3306)/test"
+
+const ttable = "keepalived_test_table"
+
+func init() {
+	if n := os.Getenv("TEST_KEEPALIVED_DSN"); n != "" {
+		dsn = n
+	}
+}
+
+type fsuite struct {
+	suite.Suite
+	db *sql.DB
+}
+
+func (s *fsuite) SetupSuite() {
+	db, err := sql.Open("mysql", dsn+"?charset=utf8&parseTime=True&loc=Local")
+	s.Require().NoError(err)
+	s.db = db
+}
+
+func (s *fsuite) TearDownSuite() {
+	_, err := s.db.Exec("DROP TABLE IF EXISTS `" + ttable + "`;")
+	s.Assert().NoError(err)
+	s.Require().NoError(s.db.Close())
+}
+
+func (s *fsuite) TestKeepalivedBase() {
+	var (
+		cfg    Config
+		kp     *Keepalived
+		assert = s.Assert()
+	)
+
+	_, err := NewKeepalived(cfg)
+	assert.Error(err)
+
+	cfg.Table = "election"
+	_, err = NewKeepalived(cfg)
+	assert.Error(err)
+
+	cfg.Election = "abcd"
+	_, err = NewKeepalived(cfg)
+	assert.Error(err)
+
+	cfg.Session = "qqq"
+	_, err = NewKeepalived(cfg)
+	assert.Error(err)
+
+	cfg.DB = s.db
+	kp, err = NewKeepalived(cfg)
+	assert.NoError(err)
+	kp.Exit()
+}
+
+func (s *fsuite) TestCreateTable() {
+	var (
+		db     string
+		count  int
+		assert = s.Assert()
+	)
+
+	hastable := func() bool {
+		assert.NoError(s.db.QueryRow("SELECT DATABASE()").Scan(&db))
+		assert.NoError(s.db.QueryRow(
+			"SELECT count(*) FROM INFORMATION_SCHEMA.TABLES WHERE table_schema = ? AND table_name = ?",
+			db, ttable).Scan(&count))
+		return count > 0
+	}
+
+	_, err := s.db.Exec("DROP TABLE IF EXISTS `" + ttable + "`;")
+	assert.NoError(err)
+	assert.False(hastable())
+	assert.NoError(CreateTable(ttable, s.db))
+	assert.True(hastable())
+	assert.NoError(CreateTable(ttable, s.db))
+	assert.True(hastable())
+}
+
+func (s *fsuite) TestKeepalivedElection() {
+	const election = "test_election"
+	var (
+		wg            sync.WaitGroup
+		election0done = make(chan struct{})
+		election1done = make(chan struct{})
+		election2done = make(chan struct{})
+		election3done = make(chan struct{})
+	)
+
+	keepalived0, err := NewKeepalived(Config{
+		Table:         ttable,
+		Election:      election,
+		Session:       "keepalived0",
+		EpochChanSize: 10,
+		DB:            s.db,
+	})
+	s.Require().NoError(err)
+
+	keepalived1, err := NewKeepalived(Config{
+		Table:         ttable,
+		Election:      election,
+		Session:       "keepalived1",
+		EpochChanSize: 10,
+		DB:            s.db,
+	})
+	s.Require().NoError(err)
+
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		n := 0
+		ch := keepalived0.Election()
+		for e := range ch {
+			switch n {
+			case 0:
+				s.Assert().Equal(LeaderChange, e.Event)
+				s.Assert().Equal("keepalived0", e.Session)
+				close(election0done)
+			case 1:
+				s.Assert().Equal(LeaderChange, e.Event)
+				s.Assert().Equal("", e.Session)
+				close(election2done)
+			}
+			n++
+		}
+	}()
+
+	select {
+	case <-election0done:
+	case <-time.After(defaultTTL):
+		s.T().Error("keepalived0 election timeout")
+	}
+
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		n := 0
+		ch := keepalived1.Election()
+		for e := range ch {
+			switch n {
+			case 0:
+				s.Assert().Equal(LeaderChange, e.Event)
+				s.Assert().Equal("keepalived0", e.Session)
+				close(election1done)
+			case 1:
+				s.Assert().Equal(LeaderChange, e.Event)
+				s.Assert().Equal("keepalived1", e.Session)
+				close(election3done)
+			}
+			n++
+		}
+	}()
+
+	select {
+	case <-election1done:
+	case <-time.After(defaultTTL):
+		s.T().Error("keepalived1 watch election timeout")
+	}
+
+	keepalived0.Resign()
+
+	select {
+	case <-election2done:
+	case <-time.After(defaultTTL):
+		s.T().Error("keepalived0 stepdown timeout")
+	}
+
+	select {
+	case <-election3done:
+	case <-time.After(defaultTTL):
+		s.T().Error("keepalived1 become leader timeout")
+	}
+
+	time.Sleep(defaultTTL)
+
+	keepalived1.Exit()
+	keepalived0.Exit()
+
+	wg.Wait()
+}
+
+func TestKeepalived(t *testing.T) {
+	suite.Run(t, &fsuite{})
+}
